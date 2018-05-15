71204beb48dca53916cf8febf517dfc204d6ba04
Dmitriy Shnitkovskiy <dm.shnit@gmail.com>
2018-05-15 18:44:29 +0300

    config

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import {DataBaseService} from './services/DataBaseService';
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://simplechat-client-web.cleverapps.io/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://simplechat-client-web.cleverapps.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-auth');
     next();
